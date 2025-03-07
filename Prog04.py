5eb8f3605e67e55e3d525463bd80371f98168b72
nykeshasalinas <salinasnykesha12@gmail.com>
2025-03-07 21:44:53 +0800

    Prog03.py

---
+++ Prog04.py
@@ -0,0 +1,6 @@
+# Prog04: Create a program that ask user to input 2 numbers. Print the product of the two numbers.
+
+num1 =  int(input("Enter first number: "))
+num2 =  int(input("Enter second number: "))
+
+print (num1 * num2)
