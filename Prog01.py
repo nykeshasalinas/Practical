9b187f527f1777c9988f19b7126e5824dec875af
nykeshasalinas <salinasnykesha12@gmail.com>
2025-03-07 21:35:05 +0800

    first commit

---
+++ Prog01.py
@@ -0,0 +1,9 @@
+# Prog01: Create a program that ask user to input 2 numbers. Print the bigger number.
+
+num1 =  int(input("Enter first number: "))
+num2 =  int(input("Enter second number: "))
+
+if num1 > num2:
+    print (num1, "is bigger than the second number.")
+else:
+    print (num2, "is bigger than the first number.")
