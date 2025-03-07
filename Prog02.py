e1f4803c879dbd6359ffab2b57cccdff28225514
nykeshasalinas <salinasnykesha12@gmail.com>
2025-03-07 21:41:45 +0800

    second commit

---
+++ Prog02.py
@@ -0,0 +1,9 @@
+# Prog02: Create a program that ask user to input 2 numbers. Print "Equal" when the numbers are the same.
+
+num1 =  int(input("Enter first number: "))
+num2 =  int(input("Enter second number: "))
+
+if num1 == num2:
+    print ("Equal")
+else:
+    print ("Not equal")
