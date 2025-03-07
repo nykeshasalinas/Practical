4119fcb15e1cf3e18b899bbed156d7614dde1b82
nykeshasalinas <salinasnykesha12@gmail.com>
2025-03-07 21:43:54 +0800

    third commit

---
+++ Prog03.py
@@ -0,0 +1,6 @@
+# Prog03: Create a program that ask user to input 2 numbers. Print the sum of the two numbers.
+
+num1 =  int(input("Enter first number: "))
+num2 =  int(input("Enter second number: "))
+
+print (num1 + num2)
